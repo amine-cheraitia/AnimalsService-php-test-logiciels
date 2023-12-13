27478f314685cdfa934fd04afc4d47c2249ca647
amine-cheraitia <amine-cheraitia@hotmail.com>
2023-12-13 15:47:43 +0100

    first version of unitTest

--- tests/unitTests/AnimalServiceUnitTest.php
+++ tests/unitTests/AnimalServiceUnitTest.php
@@ -19,6 +19,8 @@ require __DIR__ . '/../../src/AnimalService.php';
  *
  * @internal
  */
+
+/*
 final class AnimalServiceUnitTest extends TestCase {
     private $animalService;
 
@@ -46,4 +48,64 @@ final class AnimalServiceUnitTest extends TestCase {
     public function testDeleteAnimalWithTextAsId() {
     }
 
+}*/
+final class AnimalServiceUnitTest extends TestCase
+{
+    private $animalService;
+
+    public function __construct(string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->animalService = new AnimalService();
+    }
+
+    public function testCreationAnimalWithoutAnyText()
+    {
+        $this->expectException(invalidInputException::class);
+        $this->animalService->createAnimal('', '');
+    }
+
+    public function testCreationAnimalWithoutName()
+    {
+        $this->expectException(invalidInputException::class);
+        $this->animalService->createAnimal('', '123');
+    }
+
+    public function testCreationAnimalWithoutNumber()
+    {
+        $this->expectException(invalidInputException::class);
+        $this->animalService->createAnimal('Test Animal', '');
+    }
+
+    public function testSearchAnimalWithNumber()
+    {
+        $this->expectException(invalidInputException::class);
+        $this->animalService->searchAnimal(123);
+    }
+
+    public function testModifyAnimalWithInvalidId()
+    {
+        $this->expectException(invalidInputException::class);
+        $this->animalService->updateAnimal('', 'New Name', '456');
+    }
+
+    public function testDeleteAnimalWithTextAsId()
+    {
+        $this->expectException(invalidInputException::class);
+        $this->animalService->deleteAnimal('InvalidId');
+    }
+
+    public function testCreationAnimalWithValidInput()
+    {
+        $result = $this->animalService->createAnimal('Test Animal', '123');
+        $this->assertTrue($result, 'Creation of animal should return true for valid input');
+    }
+
+    public function testSearchAnimalWithValidInput()
+    {
+        $result = $this->animalService->searchAnimal('Test');
+        $this->assertIsArray($result, 'Search should return an array for valid input');
+    }
+
+    // Add more tests as needed for other methods
 }
